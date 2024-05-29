f83ea0e6a19c97285946e081669857a643cc1b9c
Lunatik3 <147321002+Lunatik3@users.noreply.github.com>
2024-05-29 17:35:09 +0500

    Update st_app.py

--- st_app.py
+++ st_app.py
@@ -1,1 +1,25 @@
+# Импорт библиотек
+import streamlit as st
+import requests
 
+# Создание Streamlit приложения
+st.title('Model')
+
+def get_result(file):
+    files = {
+        'file': (file.name, file.getvalue(), file.type),
+        'Content-Type': 'image/jpeg',
+    }
+    response = requests.post('http://127.0.0.1:8000/run_model/', files=files)
+    if response.status_code == 500:
+        st.error('Ошибка в работе модели')
+        return
+    st.image(image=response.content)
+
+file = st.file_uploader(label='Загрузите файл', type=['jpeg', 'jpg', 'webp', 'png'])
+
+if file:
+    st.button(label='Запустить модель', on_click=get_result, args=(file,))
+
+if __name__ == '__main__':
+    uvicorn.run(host='0.0.0.0', port=8000, app="main:app")
