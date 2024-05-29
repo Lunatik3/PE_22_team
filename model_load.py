9037d33894929474771cd31301e82ea17bf0c7a7
SArtemA <148715539+SArtemA@users.noreply.github.com>
2024-05-29 18:23:53 +0500

    Update model_load.py

--- model_load.py
+++ model_load.py
@@ -1,5 +1,6 @@
 from ultralytics import YOLO
 import gdown
+import os
 
 if 'best.pt' not in os.listdir():
   gdown.download(url='https://drive.google.com/file/d/1nolYX5CFPJWp8BcyPscUp-urKQ3jTTf-/view?usp=drive_link', fuzzy=True)
