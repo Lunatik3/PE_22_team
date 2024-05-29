a190bbe0204eaa227351e82de31b857ead7fcef1
Lunatik3 <147321002+Lunatik3@users.noreply.github.com>
2024-05-29 17:43:51 +0500

    Update test_api.py

--- test_api.py
+++ test_api.py
@@ -6,8 +6,8 @@ BASE_URL = "http://127.0.0.1:8000"
 @pytest.mark.asyncio
 async def test_run_model():
     # Убедитесь, что у вас есть изображение для теста
-    with open("test_image.jpg", "rb") as file:
-        files = {"file": ("test_image.jpg", file, "image/jpeg")}
+    with open("images_test/помидоры.jpg", "rb") as file:
+        files = {"file": ("помидоры.jpg", file, "image/jpeg")}
         
         async with httpx.AsyncClient() as client:
             response = await client.post(f"{BASE_URL}/run_model/", files=files)
